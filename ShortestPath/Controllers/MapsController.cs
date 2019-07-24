d4d494477733571ec758d504f6ca4bef093fa15f
brandon.whitfield <brandon.whitfield@asynchrony.com>
2019-07-24 13:23:56 -0500

    starting point - bw

---
+++ ShortestPath/Controllers/MapsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShortestPath.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MapsController : ControllerBase
+    {
+        // GET maps
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET maps/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST maps
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT maps/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE maps/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ShortestPathTests/Controllers/MapsControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using ShortestPath.Controllers;
+
+namespace ShortestPathTests.Controllers
+{
+    public class MapsControllerTests
+    {
+        MapsController _testObject;
+        Mock<ILogger<MapsController>> _logger;
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Test() 
+        {
+            Assert.Fail(); 
+        }
+
+    }
+}
