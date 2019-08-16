754de639af757e373313c8bc17158c894f432554
B Whitfield <bwhitfield@github.com>
2019-08-16 12:44:14 -0500

    1 - map controller passes input to facade - bw rk

---
+++ ShortestPath/Facades/MapFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ShortestPath.Models;
+
+namespace ShortestPath.Facades
+{
+    public interface IMapFacade
+    {
+        void SaveMap(string mapId, ViewMap viewMap);
+    }
+
+    public class MapFacade : IMapFacade
+    {
+        public MapFacade()
+        {
+        }
+
+        public void SaveMap(string mapId, ViewMap viewMap)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
