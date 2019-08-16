059c5c8bf3bbca98b470a1bd998ddc1c4f71ef7f
B Whitfield <bwhitfield@github.com>
2019-08-16 13:53:39 -0500

    1 - create adjacency matrix - bw bw

--- ShortestPath/Servies/AdjacencyMatrix.cs
+++ ShortestPath/Servies/AdjacencyMatrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using ShortestPath.Models;
 
 namespace ShortestPath.Servies
@@ -15,7 +16,34 @@ namespace ShortestPath.Servies
 
         public float[,] CreateMatrix(ViewNodes viewNodes)
         {
-            throw new NotImplementedException();
+            var nodes = viewNodes.Nodes;
+            var keys = nodes.Select(x => x.Id).ToList();
+
+            var ct = nodes.Count;
+            float[,] matrix = new float[ct, ct];
+
+            for (int i = 0; i < ct; i++)
+            {
+                for (int j = 0; j < ct; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+
+            }
+
+
+            for (int i = 0; i < ct; i++)
+            {
+                var node = nodes[i];
+                foreach(var arc in node.Arc)
+                {
+                    matrix[i, keys.IndexOf(arc.Key)] = arc.Value;
+                }
+
+            }
+
+
+            return matrix;
         }
     }
 
